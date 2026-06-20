68781510b7c8c74109b82744ac6f88d7266739e2
FraserFallows <fraserfallows@hotmail.co.uk>
2026-06-20 13:39:33 +0100

    Migrate to Eleventy with GitHub Actions deployment

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+﻿module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("script.js");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("CNAME");
+    eleventyConfig.addPassthroughCopy("admin.html");
+
+    return {
+        htmlTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    };
+};
